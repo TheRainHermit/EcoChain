875e98810c6ed96428db68f5a644bdf50997845b
TheRainHermit <miguel.fabra211294@gmail.com>
2025-08-27 12:14:48 -0500

    Despliegue 2: Api serverless

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -20,7 +20,7 @@ const ContactPage: React.FC = () => {
     };
 
     try {
-      const res = await fetch("http://localhost:4000/api/contact", {
+      const res = await fetch("http://ecochain-landing.vercel.app/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
